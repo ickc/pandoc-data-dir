4f026f07473f4de3a19723c595dd748cba6cb535
KolenCheung <christian.kolen@gmail.com>
2016-10-06 04:46:16 -0700

    bugs fixed

--- bin/pandoc.sh
+++ bin/pandoc.sh
@@ -37,7 +37,7 @@ DIRECTORY=${PATHNAME%/*}
 # ext="${EXT,,}" #This does not work on Mac's default, old version of, bash.
 
 # define commonly used pandoc arg
-fromMarkdown="markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash"
+fromMarkdown="markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash-fancy_lists"
 argFromMarkdown="-f $fromMarkdown -S --base-header-level=1 --toc --toc-depth=6 -N"
 toMarkdown="markdown-simple_tables-multiline_tables-grid_tables"
 argToMarkdown="-t $toMarkdown --wrap=none --atx-headers --extract-media=\"$PATHNAMEWOEXT\""
@@ -67,8 +67,8 @@ if [ $pandoc_biblatex = true ]; then
 fi
 
 # output
-if [ "$EXT" = "md" ] && ([ "to_format" = "html" ] || [ "to_format" = "tex" ] || [ "to_format" = "pdf" ]);  then
-	FILE=$(pandoc-criticmarkup.sh -d $to_format "$PATHNAME") # preprocess and read to a var
+if [ "$EXT" = "md" ] && ([ "$to_format" = "html" ] || [ "$to_format" = "tex" ] || [ "$to_format" = "pdf" ]);  then
+	FILE=$(cat "$PATHNAME" | pandoc-criticmarkup.sh -d $to_format) # preprocess and read to a var
 	if [ "$to_format" = "html" ]; then
 		echo "$FILE" | pandoc $arg -o "$PATHNAMEWOEXT.$to_format" -H <(echo "$FILE" | pandoc --template=$HOME/.pandoc/includes/default.html)
 	elif [ "$to_format" = "tex" ] || [ "$to_format" = "pdf" ]; then
