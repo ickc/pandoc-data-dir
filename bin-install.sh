568656fa69ad9dcecf65386d12d7fe7d56511e04
Kolen Cheung <christian.kolen@gmail.com>
2016-11-04 03:37:20 -0700

    use `$HOME` instead of `~`

--- bin-install.sh
+++ bin-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-printf "%s\n" "" "# pandoc-data-dir PATH" "export PATH=\"$DIR/bin:"'$PATH"' "export PATH=\"$DIR/filters:"'$PATH"' >> ~/.bash_profile
+printf "%s\n" "" "# pandoc-data-dir PATH" "export PATH=\"$DIR/bin:"'$PATH"' "export PATH=\"$DIR/filters:"'$PATH"' >> $HOME/.bash_profile
