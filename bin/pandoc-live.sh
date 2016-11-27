a8314fd5b9f4418d028553aebc24c83be67361b7
Kolen Cheung <christian.kolen@gmail.com>
2016-11-27 01:25:07 -0800

    pandoc-live.sh: reorganize and use long-form arg

--- bin/pandoc-live.sh
+++ bin/pandoc-live.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pandoc --normalize -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash-fancy_lists -S --base-header-level=1 --toc --toc-depth=6 -N --mathjax -H ~/.pandoc/submodule/markdown-latex-css/js/mathjax/setup-mathjax-cdn.html
+pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash-fancy_lists \
+--smart --number-sections --toc --toc-depth=6 --normalize --mathjax --include-in-header=~/.pandoc/submodule/markdown-latex-css/js/mathjax/setup-mathjax-cdn.html --filter=pandoc-amsthm.py
