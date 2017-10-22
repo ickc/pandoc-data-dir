2ac01d8a0db93f445dcf1566e774c86107d9fb58
Kolen <christian.kolen@gmail.com>
2017-10-22 15:35:22 -0700

    update shebang to use env's bash

--- bin-install.sh
+++ bin-install.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 printf "%s\n" "" "# pandoc-data-dir PATH" "export PATH=\"$DIR/bin:"'$PATH"' "export PATH=\"$DIR/filters:"'$PATH"' >> $HOME/.bash_profile
--- bin/pandoc-live.sh
+++ bin/pandoc-live.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 pandoc -f markdown+abbreviations+autolink_bare_uris+markdown_attribute+mmd_header_identifiers+mmd_link_attributes+mmd_title_block+tex_math_double_backslash-fancy_lists \
 --smart --number-sections --toc --toc-depth=6 --normalize --mathjax --include-in-header=~/.pandoc/submodule/markdown-latex-css/js/mathjax/setup-mathjax-cdn.html --filter=pandoc-amsthm.py
